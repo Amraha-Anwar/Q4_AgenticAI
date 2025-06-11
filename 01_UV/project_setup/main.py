25a4e61b54700a32f515e16a7589566c25c66964
Amraha-Anwar <amrahaanwar@gmail.com>
2025-06-11 17:40:23 +0500

    learn learn learn

---
+++ 01_UV/project_setup/main.py
@@ -0,0 +1,16 @@
+def main():
+    print("Hello from 01-uv!")
+
+
+if __name__ == "__main__":
+    main()
+
+
+# STEP 1: uv version (to check if uv is installed or not)
+# STEP 2: uv init (for the existing folder) --> uv init 'foldername' (to initialize a folder from scratch using uv)
+# STEP 3: uv venv (to create virtual environment)
+# STEP 4: .venv\Scripts\activate (to activate virtual environment)
+# STEP 5: uv run [filename] (to run your code)
+
+
+# and here you gooo!!!
