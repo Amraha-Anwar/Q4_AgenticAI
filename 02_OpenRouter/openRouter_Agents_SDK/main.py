3c11ca48a4a0deec47b493a386d98b3cde5d5a55
Amraha-Anwar <amrahaanwar@gmail.com>
2025-06-12 17:57:29 +0500

    fixed version

--- 02_OpenRouter/openRouter_Agents_SDK/main.py
+++ 02_OpenRouter/openRouter_Agents_SDK/main.py
@@ -1,10 +1,10 @@
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 import os
 import json
 import requests
 
 
-load_dotenv() 
+load_dotenv(find_dotenv()) 
 
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
 MODEL = os.getenv("MODEL")
@@ -12,7 +12,7 @@ BASE_URL = os.getenv("BASE_URL")
 URL = os.getenv("URL")
 
 response = requests.post(
-    url = f"{URL}/chat/completion",
+    url = f"{URL}/chat/completions",
     headers = {
         "Authorization" : f"Bearer {OPENROUTER_API_KEY}"
     },
@@ -28,3 +28,21 @@ response = requests.post(
 )
 
 print(response.json()['choices'][0]['message']['content'])
+
+
+# OUTPUT 👇🏻
+# The **national language of Pakistan** is **Urdu**.  
+
+# However, **Pakistan recognizes multiple official languages**, including:  
+# - **Urdu** (National language and lingua franca)  
+# - **English** (Official and widely used in government, education, and business)  
+
+# Additionally, several **regional languages** are spoken across Pakistan, such as:  
+# - Punjabi  
+# - Sindhi  
+# - Pashto  
+# - Balochi  
+# - Saraiki
+# - And others
+
+# While Urdu serves as the national symbol of unity, different provinces often use their native languages in daily communication.
