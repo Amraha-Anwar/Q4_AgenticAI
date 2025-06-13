b82bcc43e0359a7693ed50365a155f99989477d3
Amraha-Anwar <amrahaanwar@gmail.com>
2025-06-14 02:53:43 +0500

    Learn learn learn

--- 03_Litellm/02_litellm_project/project/src/project/hello_llm.py
+++ 03_Litellm/02_litellm_project/project/src/project/hello_llm.py
@@ -12,11 +12,11 @@ def gemini():
     print(response)
 
 # OUTPUT 👇🏻
-# ModelResponse(id='N5xMaJH1GsuM1PIPkIuSsAI', created=1749851190, model='gemini-2.0-flash', object='chat.completion', system_fingerprint=None, choices=[Choices(finish_reason='stop', index=0, message=Message
-# -----------------------------------------------------------------------
-# (content='I am doing well, thank you for asking! How are you today?\n', 
-#  -----------------------------------------------------------------------
-#  role='assistant', tool_calls=None, function_call=None, provider_specific_fields=None))], usage=Usage(completion_tokens=16, prompt_tokens=5, total_tokens=21, completion_tokens_details=None, prompt_tokens_details=PromptTokensDetailsWrapper(audio_tokens=None, cached_tokens=None, text_tokens=5, image_tokens=None)), vertex_ai_grounding_metadata=[], vertex_ai_url_context_metadata=[], vertex_ai_safety_results=[], vertex_ai_citation_metadata=[])
+""" ModelResponse(id='N5xMaJH1GsuM1PIPkIuSsAI', created=1749851190, model='gemini-2.0-flash', object='chat.completion', system_fingerprint=None, choices=[Choices(finish_reason='stop', index=0, message=Message
+ -----------------------------------------------------------------------
+ (content='I am doing well, thank you for asking! How are you today?\n', 
+  -----------------------------------------------------------------------
+  role='assistant', tool_calls=None, function_call=None, provider_specific_fields=None))], usage=Usage(completion_tokens=16, prompt_tokens=5, total_tokens=21, completion_tokens_details=None, prompt_tokens_details=PromptTokensDetailsWrapper(audio_tokens=None, cached_tokens=None, text_tokens=5, image_tokens=None)), vertex_ai_grounding_metadata=[], vertex_ai_url_context_metadata=[], vertex_ai_safety_results=[], vertex_ai_citation_metadata=[]) """
 
 
 def gemini2():
@@ -30,8 +30,8 @@ def gemini2():
 
 # OUTPUT 👇🏻
 
-# ModelResponse(id='eZxMaOyAHtixgLUP29rU0QE', created=1749851256, model='gemini-2.0-flash-exp', object='chat.completion', system_fingerprint=None, choices=[Choices(finish_reason='stop', index=0, message=Message
-# ---------------------------------------------------------------------------------------------------
-# content="I am doing well, thank you for asking! As a large language model, I don't experience emotions or have feelings in the same way humans do, but I am f for asking! As a large language model, I don't experience emotions or have feelings in the same way humans do, but I am functioning optimally and ready to assist you. How can I help you today?\n", 
-# ----------------------------------------------------------------------------------------------------
-# role='assistant', tool_calls=None, function_caunctioning optimally and ready to assist you. How can I help you today?\n", role='assistant', tool_calls=None, function_call=None, provider_specific_fields=None))], usage=Usage(completion_tokens=51, prompt_tokens=6, total_tokens=57, completion_tokens_details=None, prompt_tokens_details=PromptTokensDetailsWrapper(audio_tokens=None, cached_tokens=None, text_tokens=6, image_tokens=None)), vertex_ai_grounding_metadata=[], vertex_ai_url_context_metadata=[], vertex_ai_safety_results=[], vertex_ai_citation_metadata=[])
+""" ModelResponse(id='eZxMaOyAHtixgLUP29rU0QE', created=1749851256, model='gemini-2.0-flash-exp', object='chat.completion', system_fingerprint=None, choices=[Choices(finish_reason='stop', index=0, message=Message
+ ---------------------------------------------------------------------------------------------------
+ content="I am doing well, thank you for asking! As a large language model, I don't experience emotions or have feelings in the same way humans do, but I am f for asking! As a large language model, I don't experience emotions or have feelings in the same way humans do, but I am functioning optimally and ready to assist you. How can I help you today?\n", 
+ ----------------------------------------------------------------------------------------------------
+ role='assistant', tool_calls=None, function_caunctioning optimally and ready to assist you. How can I help you today?\n", role='assistant', tool_calls=None, function_call=None, provider_specific_fields=None))], usage=Usage(completion_tokens=51, prompt_tokens=6, total_tokens=57, completion_tokens_details=None, prompt_tokens_details=PromptTokensDetailsWrapper(audio_tokens=None, cached_tokens=None, text_tokens=6, image_tokens=None)), vertex_ai_grounding_metadata=[], vertex_ai_url_context_metadata=[], vertex_ai_safety_results=[], vertex_ai_citation_metadata=[]) """
